5fbc257fadba013b0843da46045f3113b9b56eec
pikesley <sam.pikesley@gmail.com>
2022-11-08 22:35:56 +0000

    Pull and render a GH README!

--- p.org/src/config.ts
+++ p.org/src/config.ts
@@ -2,3 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'sam.pikesley.org';
+export const SITE_DESCRIPTION = 'Fucking JavaScript';
