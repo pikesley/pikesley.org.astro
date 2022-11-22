598a2e2f4726971c6eea32f89a479f70d22cac89
pikesley <sam.pikesley@gmail.com>
2022-11-22 22:35:46 +0000

    Some vscode reformatting

--- p.org/astro.config.mjs
+++ p.org/astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
