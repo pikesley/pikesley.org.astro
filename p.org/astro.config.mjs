571347b16b947069431cc363c9322fc1db59f515
pikesley <sam.pikesley@gmail.com>
2022-11-30 22:45:04 +0000

    Add beautifier

--- p.org/astro.config.mjs
+++ p.org/astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sam.pikesley.org",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    htmlBeautifier({
+      indent_size: 2,
+      wrap_line_length: 88,
+      jslint_happy: true,
+      preserve_newlines: false,
+    }),
+  ],
 });
